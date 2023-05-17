8924f8ec44c5ba304070057359a00c2148e7ed6c
Renan Santos Gomes <github@renansgomes.com.br>
2023-05-17 15:26:14 -0300

    Iniciando o projeto

---
+++ src/main/java/com/renansgomes/learnspringboot/learnspringbootjdbcjpa/LearnSpringbootJdbcJpaApplication.java
@@ -0,0 +1,13 @@
+package com.renansgomes.learnspringboot.learnspringbootjdbcjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringbootJdbcJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringbootJdbcJpaApplication.class, args);
+	}
+
+}
