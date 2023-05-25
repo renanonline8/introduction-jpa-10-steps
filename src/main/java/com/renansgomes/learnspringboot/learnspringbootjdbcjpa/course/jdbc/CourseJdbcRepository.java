43b8aa14d0702995b72b5a1c2516d8880166a1c8
Renan Santos Gomes <github@renansgomes.com.br>
2023-05-25 16:44:21 -0300

    Step 07 - Querying Data using Spring JDBC

--- src/main/java/com/renansgomes/learnspringboot/learnspringbootjdbcjpa/course/jdbc/CourseJdbcRepository.java
+++ src/main/java/com/renansgomes/learnspringboot/learnspringbootjdbcjpa/course/jdbc/CourseJdbcRepository.java
@@ -1,6 +1,7 @@
 package com.renansgomes.learnspringboot.learnspringbootjdbcjpa.course.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -25,11 +26,21 @@ public class CourseJdbcRepository {
 				delete from course where id = ?
 			""";
 	
+	private static String SELECT_QUERY =
+			
+			"""
+				select * from course where id = ?	
+			""";
+	
 	public void insert(Course course) {
 		springJdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
 	}
 	
-	public void delete(int id) {
+	public void delete(long id) {
 		springJdbcTemplate.update(DELETE_QUERY, id);
+	}
+	
+	public Course findById(long id) {
+		return springJdbcTemplate.queryForObject(SELECT_QUERY, new BeanPropertyRowMapper<>(Course.class), id);
 	}
 }
