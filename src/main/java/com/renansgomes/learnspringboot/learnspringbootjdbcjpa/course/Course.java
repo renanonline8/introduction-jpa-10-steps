f38f6de8458601167bcfc4fd72f64f5e5537043b
Renan Santos Gomes <github@renansgomes.com.br>
2023-05-25 17:11:28 -0300

    Step 08 - Getting Started with JPA and EntityManager

--- src/main/java/com/renansgomes/learnspringboot/learnspringbootjdbcjpa/course/Course.java
+++ src/main/java/com/renansgomes/learnspringboot/learnspringbootjdbcjpa/course/Course.java
@@ -1,6 +1,12 @@
 package com.renansgomes.learnspringboot.learnspringbootjdbcjpa.course;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Course {
+	
+	@Id
 	private long id;
 	private String name;
 	private String author;
