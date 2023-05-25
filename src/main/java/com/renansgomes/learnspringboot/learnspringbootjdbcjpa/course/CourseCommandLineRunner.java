da1027e1f23b653a5794a34c04c09597f3d173ac
Renan Santos Gomes <github@renansgomes.com.br>
2023-05-25 17:45:29 -0300

    Step 11 - Understanding difference between Hibernate and JPA

--- src/main/java/com/renansgomes/learnspringboot/learnspringbootjdbcjpa/course/CourseCommandLineRunner.java
+++ src/main/java/com/renansgomes/learnspringboot/learnspringbootjdbcjpa/course/CourseCommandLineRunner.java
@@ -5,23 +5,30 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.renansgomes.learnspringboot.learnspringbootjdbcjpa.course.jpa.CourseJpaRepository;
+import com.renansgomes.learnspringboot.learnspringbootjdbcjpa.course.springJpa.CourseSpringJpaRepository;
 
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner {
 	
 	@Autowired
-	private CourseJpaRepository repository;
+	private CourseSpringJpaRepository repository;
 
 	@Override
 	public void run(String... args) throws Exception {
-		repository.insert(new Course(1, "Learn AWS JPA!", "in20minutes"));
-		repository.insert(new Course(2, "Learn Azure JPA!", "in20minutes"));
-		repository.insert(new Course(3, "Learn DevOps JPA!", "in20minutes"));
+		repository.save(new Course(1, "Learn AWS JPA!", "in20minutes"));
+		repository.save(new Course(2, "Learn Azure JPA!", "in20minutes"));
+		repository.save(new Course(3, "Learn DevOps JPA!", "in20minutes"));
 		
-		repository.delete(3);
+		repository.deleteById(3l);
 		
-		System.out.println(repository.findById(1));
-		System.out.println(repository.findById(2));
+		System.out.println(repository.findById(1l));
+		System.out.println(repository.findById(2l));
+		
+		System.out.println(repository.findAll());
+		System.out.println("Qtd Courses: " + repository.count());
+		
+		System.out.println(repository.findByAuthor("in20minutes"));
+		System.out.println(repository.findByName("Learn Azure JPA!"));
 	}
 	
 }
--- src/main/java/com/renansgomes/learnspringboot/learnspringbootjdbcjpa/course/springJpa/CourseSpringJpaRepository.java
+++ src/main/java/com/renansgomes/learnspringboot/learnspringbootjdbcjpa/course/springJpa/CourseSpringJpaRepository.java
@@ -1,9 +1,14 @@
 package com.renansgomes.learnspringboot.learnspringbootjdbcjpa.course.springJpa;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.renansgomes.learnspringboot.learnspringbootjdbcjpa.course.Course;
 
 public interface CourseSpringJpaRepository extends JpaRepository<Course, Long> {
-
+	
+	public List<Course> findByAuthor(String author);
+	
+	public List<Course> findByName(String name);
 }
